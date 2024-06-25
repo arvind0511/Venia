7ce479a5a71935be90a217279ad24fc1494c610f
arvind0511 <arvinddhayal0511@gmail.com>
2024-06-25 16:45:38 +0530

    Ui mobile responsive and lazy loader

--- assets/scripts/index.js
+++
@@ -1,5 +0,0 @@
-function redirectTo(index) {
-    var searchParams = new URLSearchParams(window.location.search)
-    searchParams.set('id', index)
-    window.location.search = searchParams.toString()
-}
